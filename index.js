25c1e143840705449cacead8594290602f72d054
vivekraman <vivekraman111@gmail.com>
2022-05-15 11:52:48 +0530

    v1.0.2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var toRgb = require('hsl-to-rgb-for-reals')
+var debug = require('debug')('hsl-to-hex')
 
 function cycle(val){
+	debug('resolving ' + val + ' within the 0-359 range')
 	return val >= 0 ? val % 360 : 360 + (val % 360)
 }
 
 function minmax(val, min, max){
+	debug('ensuring ' + val + ' is no more than ' + max + ' and no less than ' + min)
 	return Math.min(Math.max(val, min), max)
 }
 
